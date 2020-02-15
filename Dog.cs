e9883c4db2796592bc0b74c881a6ead293cc4920
NoahKirsch20 <54324590+NoahKirsch20@users.noreply.github.com>
2020-02-14 20:07:38 -0600

    Update Dog.cs

--- Dog.cs
+++ Dog.cs
@@ -9,5 +9,25 @@ namespace Dog
     public int age;
     public Gender gender;
     
+    public Dog(string name, string owner, int age, Gender gender)
+    {
+       this.name = name;
+       this.owner = owner;
+       this.age = age;
+       this.gender = gender;
+    }
+    
+    public class Bark(int barkAmount)
+    {
+      for (int 1 = 0; i < barkAmount; i++)
+      {
+        Console.Write("Woof!");
+      }
+    }
+    public class GetTag
+    {
+        console.writeLine("If lost call {0}, {1} name is {2} and she is {3} {4} old.", owner, gender, name, age, yrs);
+        console.ReadLine();
+    }
   }
 }
